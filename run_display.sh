0342d2e7b20d97f0d2baf1bc26219b7670419875
Michael Lingelbach <m.j.lbach@gmail.com>
2021-07-14 16:47:23 -0700

    Add minimal example

---
+++ run_display.sh
@@ -0,0 +1,7 @@
+podman build -f Dockerfile.minimal -t cuda-test-1 && \
+podman run --rm -it --net=host \
+--security-opt=no-new-privileges \
+--security-opt label=type:nvidia_container_t \
+-e DISPLAY \
+-v $HOME:$HOME \
+cuda-test-1:latest bash
